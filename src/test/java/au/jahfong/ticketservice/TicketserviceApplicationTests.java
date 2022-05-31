2799ae74c9c3861e1a46e0c596b86a8cedf1158f
john.ahfong <john.ahfong@gmail.com>
2022-05-31 12:59:29 +1000

    init springboot project using spring initialzr

---
+++ src/test/java/au/jahfong/ticketservice/TicketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.jahfong.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
