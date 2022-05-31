b03a45ab99fba40755c2a8400e8ff68536a48f88
john.ahfong <john.ahfong@gmail.com>
2022-05-31 22:07:34 +1000

    add checkstyle and jacocco

--- src/main/java/au/jahfong/ticketservice/TicketserviceApplication.java
+++ src/main/java/au/jahfong/ticketservice/TicketserviceApplication.java
@@ -3,11 +3,14 @@ package au.jahfong.ticketservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point of the application.
+ */
 @SpringBootApplication
 public class TicketserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketserviceApplication.class, args);
+    }
 
 }
